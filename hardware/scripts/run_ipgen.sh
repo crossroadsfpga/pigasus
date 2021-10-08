1125a16e6870049744a49f6ce5d6d615cb405e48
Zhipeng Zhao <zzhao1@andrew.cmu.edu>
2021-10-07 22:17:51 -0400

    fix device number

--- hardware/scripts/run_ipgen.sh
+++ hardware/scripts/run_ipgen.sh
@@ -23,11 +23,11 @@ qsys-script --script=eth.tcl
 #generate example design
 qsys-generate ip_gen_alt_ehipc2_0.ip -example-design
 cd ip_gen_alt_ehipc2_0_example_design/alt_ehipc2_0_example_design/hardware_test_design/common/
-qsys-generate reset_ip.ip --synthesis=VERILOG --part=1SMBHU2F53E1VG 
-qsys-generate alt_ehipc2_jtag_avalon.ip --synthesis=VERILOG --part=1SMBHU2F53E1VG 
-qsys-generate alt_ehipc2_sys_pll.ip --synthesis=VERILOG --part=1SMBHU2F53E1VG
-qsys-generate probe8.ip --synthesis=VERILOG --part=1SMBHU2F53E1VG 
-qsys-generate reset_ip.ip --synthesis=VERILOG --part=1SMBHU2F53E1VG 
+qsys-generate reset_ip.ip --synthesis=VERILOG --part=1SM21BHU2F53E1VG 
+qsys-generate alt_ehipc2_jtag_avalon.ip --synthesis=VERILOG --part=1SM21BHU2F53E1VG 
+qsys-generate alt_ehipc2_sys_pll.ip --synthesis=VERILOG --part=1SM21BHU2F53E1VG
+qsys-generate probe8.ip --synthesis=VERILOG --part=1SM21BHU2F53E1VG 
+qsys-generate reset_ip.ip --synthesis=VERILOG --part=1SM21BHU2F53E1VG 
 cd ../../../../../
 
 
